75d711fa2fd46ad95497878e5c2e2c3e1732b41c
Anh Khoa Nguyễn <oggyanhkhoa123@gmail.com>
2022-11-28 20:25:33 +0700

    redesign FormMain.cs to look more better

--- WindowsFormsFinalSE/FormMain.Designer.cs
+++ WindowsFormsFinalSE/FormMain.Designer.cs
@@ -34,8 +34,19 @@
             this.iconImport = new FontAwesome.Sharp.IconButton();
             this.iconAccountant = new FontAwesome.Sharp.IconButton();
             this.Logo = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.PictureBox();
             this.MainPanel = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.lblTitleChildForm = new System.Windows.Forms.Label();
+            this.iconCurrentChildForm = new FontAwesome.Sharp.IconPictureBox();
+            this.panelShadow = new System.Windows.Forms.Panel();
+            this.panelForm = new System.Windows.Forms.Panel();
             this.MenuPanel.SuspendLayout();
+            this.Logo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
+            this.MainPanel.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).BeginInit();
             this.SuspendLayout();
             // 
             // MenuPanel
@@ -51,7 +62,6 @@
             this.MenuPanel.Name = "MenuPanel";
             this.MenuPanel.Size = new System.Drawing.Size(200, 544);
             this.MenuPanel.TabIndex = 1;
-            this.MenuPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MenuPanel_Paint);
             // 
             // iconReport
             // 
@@ -72,6 +82,7 @@
             this.iconReport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconReport.UseVisualStyleBackColor = true;
+            this.iconReport.Click += new System.EventHandler(this.iconReport_Click);
             // 
             // iconNote
             // 
@@ -92,6 +103,7 @@
             this.iconNote.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconNote.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconNote.UseVisualStyleBackColor = true;
+            this.iconNote.Click += new System.EventHandler(this.iconNote_Click);
             // 
             // iconImport
             // 
@@ -112,6 +124,7 @@
             this.iconImport.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconImport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconImport.UseVisualStyleBackColor = true;
+            this.iconImport.Click += new System.EventHandler(this.iconImport_Click);
             // 
             // iconAccountant
             // 
@@ -136,19 +149,86 @@
             // 
             // Logo
             // 
+            this.Logo.Controls.Add(this.btnHome);
             this.Logo.Dock = System.Windows.Forms.DockStyle.Top;
             this.Logo.Location = new System.Drawing.Point(0, 0);
             this.Logo.Name = "Logo";
             this.Logo.Size = new System.Drawing.Size(200, 143);
             this.Logo.TabIndex = 0;
             // 
+            // btnHome
+            // 
+            this.btnHome.Image = global::WindowsFormsFinalSE.Properties.Resources.LogoTDTBgWhite;
+            this.btnHome.Location = new System.Drawing.Point(12, 30);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(166, 76);
+            this.btnHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnHome.TabIndex = 0;
+            this.btnHome.TabStop = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
             // MainPanel
             // 
+            this.MainPanel.Controls.Add(this.panelForm);
+            this.MainPanel.Controls.Add(this.panelShadow);
+            this.MainPanel.Controls.Add(this.panelTitleBar);
             this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainPanel.Location = new System.Drawing.Point(200, 0);
             this.MainPanel.Name = "MainPanel";
             this.MainPanel.Size = new System.Drawing.Size(902, 544);
             this.MainPanel.TabIndex = 2;
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.panelTitleBar.Controls.Add(this.lblTitleChildForm);
+            this.panelTitleBar.Controls.Add(this.iconCurrentChildForm);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(902, 75);
+            this.panelTitleBar.TabIndex = 0;
+            // 
+            // lblTitleChildForm
+            // 
+            this.lblTitleChildForm.AutoSize = true;
+            this.lblTitleChildForm.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblTitleChildForm.Location = new System.Drawing.Point(72, 30);
+            this.lblTitleChildForm.Name = "lblTitleChildForm";
+            this.lblTitleChildForm.Size = new System.Drawing.Size(44, 16);
+            this.lblTitleChildForm.TabIndex = 1;
+            this.lblTitleChildForm.Text = "Home";
+            // 
+            // iconCurrentChildForm
+            // 
+            this.iconCurrentChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.iconCurrentChildForm.ForeColor = System.Drawing.Color.MediumOrchid;
+            this.iconCurrentChildForm.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.iconCurrentChildForm.IconColor = System.Drawing.Color.MediumOrchid;
+            this.iconCurrentChildForm.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconCurrentChildForm.Location = new System.Drawing.Point(34, 30);
+            this.iconCurrentChildForm.Name = "iconCurrentChildForm";
+            this.iconCurrentChildForm.Size = new System.Drawing.Size(32, 32);
+            this.iconCurrentChildForm.TabIndex = 0;
+            this.iconCurrentChildForm.TabStop = false;
+            // 
+            // panelShadow
+            // 
+            this.panelShadow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(24)))), ((int)(((byte)(58)))));
+            this.panelShadow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelShadow.Location = new System.Drawing.Point(0, 75);
+            this.panelShadow.Name = "panelShadow";
+            this.panelShadow.Size = new System.Drawing.Size(902, 9);
+            this.panelShadow.TabIndex = 1;
+            // 
+            // panelForm
+            // 
+            this.panelForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.panelForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelForm.Location = new System.Drawing.Point(0, 84);
+            this.panelForm.Name = "panelForm";
+            this.panelForm.Size = new System.Drawing.Size(902, 460);
+            this.panelForm.TabIndex = 2;
             // 
             // FormMain
             // 
@@ -161,6 +241,12 @@
             this.Name = "FormMain";
             this.Text = "Supplement Facts Products Management";
             this.MenuPanel.ResumeLayout(false);
+            this.Logo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).EndInit();
+            this.MainPanel.ResumeLayout(false);
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -174,6 +260,12 @@
         private FontAwesome.Sharp.IconButton iconAccountant;
         private System.Windows.Forms.Panel Logo;
         private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox btnHome;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private FontAwesome.Sharp.IconPictureBox iconCurrentChildForm;
+        private System.Windows.Forms.Label lblTitleChildForm;
+        private System.Windows.Forms.Panel panelForm;
+        private System.Windows.Forms.Panel panelShadow;
     }
 }
 
--- WindowsFormsFinalSE/FormMain.cs
+++ WindowsFormsFinalSE/FormMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,20 +13,116 @@ namespace WindowsFormsFinalSE
 {
     public partial class FormMain : Form
     {
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+        private Form currentChildForm;
         public FormMain()
         {
             InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            MenuPanel.Controls.Add(leftBorderBtn);
         }
 
-        private void MenuPanel_Paint(object sender, PaintEventArgs e)
+        private void ActivateButton(object senderBtn, Color color)
         {
+            if (senderBtn != null)
+            {
+                DisableButton();
+                //Button
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+                //Left border button
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+                //Current Child Form Icon
+                iconCurrentChildForm.IconChar = currentBtn.IconChar;
+                iconCurrentChildForm.IconColor = color;
+            }
+        }
 
+        private void DisableButton()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(31, 30, 68);
+                currentBtn.ForeColor = Color.Gainsboro;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.Gainsboro;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+
+        }
+
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            //public static Color color5 = Color.FromArgb(249, 88, 155);
+            //public static Color color6 = Color.FromArgb(24, 161, 251);
         }
 
         private void iconAccountant_Click(object sender, EventArgs e)
         {
-            FormAccountant formAccountant = new FormAccountant();
-            formAccountant.Show();
+            ActivateButton(sender, RGBColors.color1);
+            OpenChildForm(new FormAccountant());
+        }
+
+        private void iconImport_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void iconNote_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+        }
+
+        private void iconReport_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color4);
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            currentChildForm.Close();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderBtn.Visible = false;
+            iconCurrentChildForm.IconChar = IconChar.Home;
+            iconCurrentChildForm.IconColor = Color.MediumPurple;
+            lblTitleChildForm.Text = "Home";
+        }
+
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelForm.Controls.Add(childForm);
+            panelForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitleChildForm.Text = childForm.Text;
         }
     }
 }
